08005c72702a190145fe9e93374e7f737cf7a947
David Thievon <pdkfan@gmail.com>
2025-02-23 13:38:02 +0100

    refactor(script): use topiary instead of topiary fmt
    This allows users to also use other topiary sub-commands.

--- topiary-yuck.sh
+++ topiary-yuck.sh
@@ -5,4 +5,4 @@ SCRIPT_DIR="$(dirname "$(realpath "$0")")"
 export TOPIARY_CONFIG_FILE="${SCRIPT_DIR}/languages.ncl"
 export TOPIARY_LANGUAGE_DIR="${SCRIPT_DIR}/languages/"
 
-topiary fmt "$@"
+topiary "$@"
