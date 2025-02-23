cfd2032a5dea7a38badb90ad3c7cac2d8bd04b55
David Thievon <pdkfan@gmail.com>
2025-02-23 13:17:13 +0100

    feat(topiary-yuck): add topiary-yuck script

---
+++ topiary-yuck.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR="$(dirname "$(realpath "$0")")"
+
+export TOPIARY_CONFIG_FILE="${SCRIPT_DIR}/languages.ncl"
+export TOPIARY_LANGUAGE_DIR="${SCRIPT_DIR}/languages/"
+
+topiary fmt "$@"
